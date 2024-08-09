cb608e6f4efcded6a8ee8e643d5d8191d1c19733
krutikmaru <krutikmaru18@gmail.com>
2024-08-09 13:00:14 +0530

    organization

--- src/components/Home/Hero2.jsx
+++ src/components/Home/Hero2.jsx
@@ -2,6 +2,18 @@
 import React, { useState, useEffect } from "react";
 
 function Hero2() {
+  return (
+    <div className="w-screen h-screen flex flex-row justify-center items-center overflow-hidden relative ">
+      <div className="absolute bottom-0 left-0 w-full h-44 z-20 bg-gradient-to-b from-transparent to-white dark:to-[#181818]"></div>
+      <div className="w-[40%] h-full"></div>
+      <ImageSlider />
+    </div>
+  );
+}
+
+export default Hero2;
+
+const ImageSlider = () => {
   const [position, setPosition] = useState(0);
 
   // List of images to be used in the divs
@@ -16,11 +28,10 @@ function Hero2() {
   useEffect(() => {
     const interval = setInterval(() => {
       setPosition((prevPosition) => (prevPosition + 1) % images.length);
-    }, 3000);
+    }, 3500);
 
     return () => clearInterval(interval);
   }, [images.length]);
-
   const getPositions = (index) => {
     const length = images.length;
     return {
@@ -28,27 +39,23 @@ function Hero2() {
       next: index === (position + 1) % length ? "-right-28" : "-right-[150%]",
     };
   };
-
   return (
-    <div className="w-screen h-screen flex flex-row justify-center items-center overflow-hidden">
-      <div className="w-[40%] h-full"></div>
-      <div className="w-[60%] h-full relative overflow-visible">
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className={`w-full h-[150vh] -top-28 absolute -rotate-6 transition-all duration-700 ease-in-out ${
-              getPositions(index).current
-            }`}
-            style={{
-              backgroundImage: `url(${image})`,
-              backgroundSize: "cover",
-              backgroundPosition: "top",
-            }}
-          ></div>
-        ))}
-      </div>
+    <div className="w-[60%] h-full relative overflow-visible">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className={`w-full h-[150vh] -top-28 absolute -rotate-6 transition-all duration-700 ease-in-out ${
+            getPositions(index).current
+          }`}
+          style={{
+            backgroundImage: `url(${image})`,
+            backgroundSize: "cover",
+            backgroundPosition: "top",
+          }}
+        >
+          <div className="absolute bottom-0 -left-2 w-44 h-full z-20 bg-gradient-to-l from-transparent to-white dark:to-[#181818]"></div>
+        </div>
+      ))}
     </div>
   );
-}
-
-export default Hero2;
+};
